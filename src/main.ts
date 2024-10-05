2ecdaca423ba158f47ae65befa4282899d41347e
Dor Zion <dor17.10@gmail.com>
2024-10-05 21:48:29 +0300

    change cors to be before cookise parser

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
-import { AppConfigService } from './modules/core/modules/config/app-config.service';
-import * as WebSocket from 'websocket';
+import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import * as WebSocket from 'websocket';
+import { AppModule } from './app.module';
+import { AppConfigService } from './modules/core/modules/config/app-config.service';
 
 // Needed for Wasm
 globalThis.WebSocket = WebSocket.w3cwebsocket;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['https://api.kaspiano.com', 'https://dev-api.kaspiano.com', 'http://localhost:8080'];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin like mobile apps or curl requests
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    }, // Replace with your frontend domain
+    credentials: true, // Allow sending cookies with requests
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods (optional)
+    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers (optional)
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -29,21 +44,7 @@ async function bootstrap() {
   const appConfigService = app.get(AppConfigService);
 
   const port = appConfigService.getServicePort || 3000;
-  const allowedOrigins = ['https://api.kaspiano.com', 'https://dev-api.kaspiano.com', 'http://localhost:8080'];
 
-  app.enableCors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin like mobile apps or curl requests
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }, // Replace with your frontend domain
-    credentials: true, // Allow sending cookies with requests
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Specify allowed methods (optional)
-    allowedHeaders: 'Content-Type, Authorization', // Specify allowed headers (optional)
-  });
   console.log(`app running on port:::`, port);
   await app.listen(port);
 
