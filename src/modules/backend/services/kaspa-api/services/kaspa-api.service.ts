8526b755f2982aa6a5a7d49e5a10c9def32a64c8
admiral9200 <lion9200.world@gmail.com>
2024-10-27 15:15:00 +0200

    can be 2 inputs

--- src/modules/backend/services/kaspa-api/services/kaspa-api.service.ts
+++ src/modules/backend/services/kaspa-api/services/kaspa-api.service.ts
@@ -3,6 +3,7 @@ import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
 import { UtilsHelper } from 'src/modules/backend/helpers/utils.helper';
 import { IKaspaApiTransactionData } from '../model/kaspa-api-transaction-data.interface';
+import { groupBy } from 'loadsh';
 
 @Injectable()
 export class KaspaApiService {
@@ -43,12 +44,14 @@ export class KaspaApiService {
       return null;
     }
 
-    if (txnInfo.inputs.length != 1) {
+    const inputWallets = Object.keys(groupBy(txnInfo.inputs, (input) => input.previous_outpoint_address));
+
+    if (inputWallets.length != 1) {
       console.error('Incorrect inputs amount on getTransactionSender');
       return null;
     }
 
-    return txnInfo.inputs[0].previous_outpoint_address;
+    return inputWallets[0];
   }
 
   async verifyPaymentTransaction(txnId: string, senderAddr: string, receiverAddr: string, amount: bigint): Promise<boolean> {
